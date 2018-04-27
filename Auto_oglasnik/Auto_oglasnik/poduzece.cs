9c23b69f9ba107803c3f8142920eb15b9861ebde
Luka Veić <38264847+weich123@users.noreply.github.com>
2018-04-27 16:03:33 +0200

    Add files via upload

---
+++ Auto_oglasnik/Auto_oglasnik/poduzece.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Auto_oglasnik.Models
+{
+    public class poduzece
+    {
+        public int id_poduzece{ get; set; }
+        public char naziv { get; set; }
+        public string adresa { get; set; }
+        public char grad { get; set; }
+        public char drzava { get; set; }
+        public char tel { get; set; }
+        public char mob { get; set; }
+        public char email { get; set; }
+        public string url { get; set; }
+        public char kontakt_osoba { get; set; }
+        public char geo_sirina { get; set; }
+        public char geo_duzina { get; set; }
+        public string opis { get; set; }
+        public int vip { get; set; }
+        public char vip_licenca { get; set; }
+    }
+}
---
+++ Auto_oglasnik/Auto_oglasnik/poduzece_djelatnost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Auto_oglasnik.Models
+{
+    public class poduzece_djelatnost
+    {
+        public int id_poduzece { get; set; }
+        public int id_djelatnost { get; set; }
+    }
+}
