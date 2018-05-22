f0a68305417ab2417909cdcbf4f252841bdfe012
lukajakopec <38263374+lukajakopec@users.noreply.github.com>
2018-05-22 14:30:28 +0200

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nastavnik.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Login stranica";
+
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            ViewBag.Message = "Stranice admina";
+
+            return View();
+        }
+        public ActionResult Client()
+        {
+            ViewBag.Message = "Stranica korisnika";
+
+            return View();
+        }
+        public ActionResult Guest()
+        {
+            ViewBag.Message = "Stranica Gosta";
+
+            return View();
+        }
+    }
+}
