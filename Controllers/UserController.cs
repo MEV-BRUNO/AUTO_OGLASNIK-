a2123d08acd49a13e4ee93ad4a309c6a8564e2a8
lukajakopec <38263374+lukajakopec@users.noreply.github.com>
2018-05-25 17:38:15 +0200

    Update UserController.cs

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Nastavnik.Controllers
+namespace Auto_oglasnik.Controllers
 {
     public class HomeController : Controller
     {
